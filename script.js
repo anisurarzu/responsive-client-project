b1eff7313536a6e3dc45dda6d044d9cc18fe4648
Anisur Rahman <62845237+anisurarzu@users.noreply.github.com>
2021-10-07 17:07:36 +0600

    commit

--- script.js
+++ script.js
@@ -4,4 +4,34 @@ $(".slider-nav").slick({
 	asNavFor: ".slider-for",
 	dots: true,
 	focusOnSelect: true,
+	responsive: [
+		{
+			breakpoint: 1200,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+			},
+		},
+		{
+			breakpoint: 1008,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+			},
+		},
+		{
+			breakpoint: 736,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			asNavFor: ".slider-for",
+			dots: true,
+		},
+		{
+			breakpoint: 688,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			asNavFor: ".slider-for",
+			dots: true,
+		},
+	],
 });
