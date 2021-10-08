bb5bd634bf3f84a27cf56945b0053c7f9788bf14
Anisur Rahman <62845237+anisurarzu@users.noreply.github.com>
2021-10-08 18:50:10 +0600

    tablet devices

--- script.js
+++ script.js
@@ -19,9 +19,16 @@ $(".slider-nav").slick({
 				slidesToScroll: 1,
 			},
 		},
-
 		{
 			breakpoint: 768,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+			},
+		},
+
+		{
+			breakpoint: 600,
 			settings: {
 				slidesToShow: 1,
 				slidesToScroll: 1,
